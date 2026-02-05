38e748f24a52ca9acb249750db4b9549bb19c469
prasum shrestha <prasumshrestha8877@gmail.com>
2026-02-05 19:45:03 +0545

    Add hover effect for images and loader functionality
    - Introduced a new script.js file to handle image hover effects.
    - Implemented an object to map image indices to their hover state images.
    - Added event listeners for mouse enter and leave to change images on hover.
    - Included a loader that fades out after a delay on page load.

---
+++ script.js
@@ -0,0 +1,38 @@
+const item = document.querySelectorAll('.img');
+
+const hoverEffect = {
+    0: "img/items/rollover-bobba-naturel-CA-fraise.png",
+    1: "img/items/rollover-bobba-naturel-CA-limonade-fruit-du-dragon.png",
+    2: "img/items/rollover-bobba-naturel-CA-limonade-mangue.png",
+    3: "img/items/rollover-bobba-naturel-CA-melondeau.png",
+    4: "img/items/rollover-bobba-naturel-CA-peche.png",
+    5:"img/items/bullefusion_fraise_bobba_490g_rollover-1.png",
+    6:"img/items/bullefusion_mangue_bobba_490g_rollover-1.png",
+    7:"img/items/bullefusion_peche_bobba_490g_rollover-1.png"
+}
+const originalSrc = Array.from(item).map(img => img.src);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('.loader');
+    setTimeout(() => {
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }
+        , 500);
+    }, 1200);
+});
+
+console.log(item);
+item.forEach((item, index) => {
+    item.style.animationDelay = `${Math.random() * 0.5}s`;
+    item.addEventListener('mouseenter', () => {
+        item.style.animationPlayState = 'running';
+        item.src = hoverEffect[index];
+    });
+    item.addEventListener('mouseleave', () => {
+        item.style.animationPlayState = 'paused';
+        item.src = originalSrc[index];
+    });
+})
+        
