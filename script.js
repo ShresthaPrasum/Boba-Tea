7a25040ce92461bcbbe1968b0fd1d2e63f802d99
prasum shrestha <prasumshrestha8877@gmail.com>
2026-02-05 21:20:32 +0545

    Added loader functionality and  navigation functionality and update styles for improved layout

--- script.js
+++ script.js
@@ -21,6 +21,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         , 500);
     }, 1200);
+
+    const logo = document.querySelector('.logo');
+    const navContainer = document.querySelector('.nav-container');
+    const main = document.querySelector('.main');
+    const navLinks = document.querySelectorAll('.nav ul li a');
+    let active = false;
+
+    logo.addEventListener('click', () => {
+        if(!active){
+            active = true;
+            navContainer.classList.add('active');
+            main.classList.add('naved');
+        }
+        else{
+            active = false;
+            navContainer.classList.remove('active');
+            main.classList.remove('naved');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navContainer.classList.remove('active');
+            main.classList.remove('naved');
+        });
+        
+    });
 });
 
 console.log(item);
